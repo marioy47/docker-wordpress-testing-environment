bb31cba736f28c8e3cd2b8d0a9fad2d62278dab4
Mario Yepes <marioy47@gmail.com>
2023-06-17 21:19:16 -0500

    Upgrading PHP version to 8.0

--- docker/wp/msmtp-mailhost-fixer.php
+++ docker/wp/msmtp-mailhost-fixer.php
@@ -5,7 +5,7 @@
  * Description:       Prevents <a href="https://stackoverflow.com/questions/52336131/why-wont-i-send-an-email-thru-wordpress-when-i-can-send-it-thru-a-php-script">errors</a> when sending emails from localhost trough msmtp
  * Version:           1.0.0
  * Requires at least: 5.2
- * Requires PHP:      7.4
+ * Requires PHP:      8.0
  * Author:            Mario Yepes
  * Author URI:        https://marioyepes.com
  * License:           GPL v2 or later
